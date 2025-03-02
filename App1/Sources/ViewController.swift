c3e95daed7356bb3bcc2f895ad1d198621416bf2
Matt Johnson <mjohnson139@gmail.com>
2025-03-02 13:01:06 -0500

    Change App1 background to systemGray6

--- App1/Sources/ViewController.swift
+++ App1/Sources/ViewController.swift
@@ -4,11 +4,12 @@ import MonoCore
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .systemGray6  // Changed from white to match App2's style
         
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "App1 Updated: \(MonoCore.getSharedMessage())"
+        label.textColor = .systemBlue
         view.addSubview(label)
         
         NSLayoutConstraint.activate([

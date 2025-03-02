6ff2a30ee58d7149d3ab60b679e6e2aaaae61a30
Matt Johnson <mjohnson139@gmail.com>
2025-03-02 12:56:50 -0500

    Add comment to MonoCore.swift

--- MonoCore/Sources/MonoCore/MonoCore.swift
+++ MonoCore/Sources/MonoCore/MonoCore.swift
@@ -3,6 +3,7 @@
 
 import Foundation
 
+// This is the shared library for our mono repo - used by both App1 and App2
 public enum MonoCore {
     public static func getSharedMessage() -> String {
         return "Hello from MonoCore!"
